4cf40870dc33f9a2205fe60fe3354bdcc7ddd0c2
Nadezha <nadezha.m@urer.ch>
2019-06-21 08:35:17 +0200

    Card class updated

--- UNOdostres/src/Card.java
+++ UNOdostres/src/Card.java
@@ -1,4 +1,23 @@
 
 public class Card {
 
+	private Card card;
+	private int punkte;
+
+	public int getPunkte() {
+		return punkte;
+	}
+
+	public void setPunkte(int punkte) {
+		this.punkte = punkte;
+	}
+
+	public Card getCard() {
+		return card;
+	}
+
+	public void setCard(Card card) {
+		this.card = card;
+	}
+
 }
