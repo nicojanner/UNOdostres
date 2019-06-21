62b10baa529275f37624d9941aaeb79deca25fad
alenr <alenr@ALENS-LAPTOP>
2019-06-21 09:50:33 +0200

    Karten Bilder hinz.

---
+++ UNOdostres/src/ch/bzz/it/unodostres/controller/ConfigurePic.java
@@ -0,0 +1,44 @@
+package ch.bzz.it.unodostres.controller;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ConfigurePic {
+		
+		public static void main(String[] args) throws IOException {
+			URL url = ConfigurePic.class.getResource("/Cards.jpg");
+			System.out.println(url);
+			BufferedImage bigImg = ImageIO.read(url);
+
+			final int width = 32;
+			final int height = 46;
+			final int cols = 5;
+			final int rows = 10;
+			
+			BufferedImage[] sprites = new BufferedImage[rows * cols];
+
+			for (int i = 0; i < rows; i++) {
+			    for (int j = 0; j < cols; j++) {
+			        sprites[(i * cols) + j] = bigImg.getSubimage(j * width, i * height, width, height);
+			    }
+			}
+			
+			JFrame f = new JFrame();
+			JPanel p = new JPanel() {
+				@Override
+	            protected void paintComponent(Graphics g) {
+	                super.paintComponent(g);
+	                g.drawImage(sprites[32], 0, 0, null);
+	            }
+			};
+			f.add(p);
+			f.pack();
+			f.setVisible(true);
+			
+		}
+}
