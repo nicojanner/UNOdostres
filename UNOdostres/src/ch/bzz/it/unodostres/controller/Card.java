23fa7e555b1fa89505187d2713814f8b6527af91
Nadezha <nadezha.m@urer.ch>
2019-06-28 09:51:03 +0200

    stack of cards

--- UNOdostres/src/ch/bzz/it/unodostres/controller/Card.java
+++ UNOdostres/src/ch/bzz/it/unodostres/controller/Card.java
@@ -1,15 +1,22 @@
 package ch.bzz.it.unodostres.controller;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Card {
 
 	private Card card;
 	private int points;
 	private int number;
-	private Color color; 
-	
-	
+	private String color;
+
+	public Card(int number, String col) {
+
+		this.number = number;
+		this.color = col;
+	}
+
 	public int getPoints() {
 		return points;
 	}
@@ -25,5 +32,6 @@ public class Card {
 	public void setCard(Card card) {
 		this.card = card;
 	}
+	
 
 }
--- UNOdostres/src/ch/bzz/it/unodostres/controller/Game.java
+++ UNOdostres/src/ch/bzz/it/unodostres/controller/Game.java
@@ -1,13 +1,43 @@
 package ch.bzz.it.unodostres.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Game {
+
+	ArrayList<Card> stack = new ArrayList<Card>();
 	
-	private Game game;
-	
-	public Game() {
+	public static void main(String[] args) {
 		
 	}
+
+	public Game() {
+
+
+
+		String[] colors = { "red", "yellow", "green", "blue" };
+		String[] color_actionCard = {"black"};
+		int[] numbers = { 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9 };
+		int[] actionCards = { 2, 2, 4, 4 };
+
+		for (String color : color_actionCard) {
+			for (int i = 0; i < actionCards.length; i++) {
+				stack.add(new Card(numbers[i], color));
+			}
+		}
+		
+		for (String color : colors) {
+			for (int i = 0; i < numbers.length; i++) {
+				stack.add(new Card(numbers[i], color));
+			}
+		}		
+
+	}
 	
+	public void shuffle() {
+		Collections.shuffle(stack);
+	}
+
 //	public static void main(String[] args) {
 //		
 //	}
