80a136d8526fb1d1c0f39f5d158c869054e163da
dautib <46050953+dautib@users.noreply.github.com>
2019-07-05 09:55:34 +0200

    mainview editting

--- UNOdostres/src/ch/bzz/it/unodostres/view/MainView.java
+++ UNOdostres/src/ch/bzz/it/unodostres/view/MainView.java
@@ -4,12 +4,11 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.util.ArrayList;
 
-import javax.swing.BorderFactory;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 
+import ch.bzz.it.unodostres.controller.Card;
 import ch.bzz.it.unodostres.controller.Player;
 
 public class MainView extends JFrame {
@@ -25,24 +24,41 @@ public class MainView extends JFrame {
 	JLabel playerName2 = new JLabel();
 	JLabel playerName3 = new JLabel();
 	JLabel playerName4 = new JLabel();
+	JLabel tableLabel = new JLabel();
 
 	JPanel pointsPanel = new JPanel();
 	JPanel mainPanel = new JPanel();
 	JPanel gamePanel = new JPanel();
+	JPanel player1CardPanel = new JPanel();
+	JPanel player2CardPanel = new JPanel();
+	JPanel player3CardPanel = new JPanel();
+	JPanel player4CardPanel = new JPanel();
 
-	JLabel tableLabel = new JLabel();
+	private ArrayList<Card> cards1 = new ArrayList<Card>();
+	private ArrayList<Card> cards2 = new ArrayList<Card>();
+	private ArrayList<Card> cards3 = new ArrayList<Card>();
+	private ArrayList<Card> cards4 = new ArrayList<Card>();
 
 	public MainView(Player player1, Player player2, Player player3, Player player4) {
 		setTitle("UNOdostres");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
 		setSize(1612, 879);
+		player1CardPanel.setLayout(new FlowLayout());
+		player2CardPanel.setLayout(new BoxLayout(player2CardPanel, BoxLayout.Y_AXIS));
+		player3CardPanel.setLayout(new FlowLayout());
+		player4CardPanel.setLayout(new BoxLayout(player2CardPanel, BoxLayout.Y_AXIS));
+		cards1 = player1.getCards();
+		cards2 = player1.getCards();
+		cards3 = player1.getCards();
+		cards4 = player1.getCards();
+
 		pointsPanel.setLayout(new FlowLayout());
 		pointsPanel.add(pointsPlayer1);
 		pointsPanel.add(pointsPlayer2);
 		pointsPanel.add(pointsPlayer3);
 		pointsPanel.add(pointsPlayer4);
-		getContentPane().add(pointsPanel, BorderLayout.NORTH);
+		getContentPane().add(pointsPanel, BorderLayout.WEST);
 		tableLabel.setPreferredSize(new Dimension(800, 600));
 		tableLabel.setBackground(Color.WHITE);
 		tableLabel.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -53,10 +69,10 @@ public class MainView extends JFrame {
 		playerName2.setText(player2.getName());
 		playerName3.setText(player3.getName());
 		playerName4.setText(player4.getName());
-		pointsPlayer1.setText(player1.getPoints() + " Points");
-		pointsPlayer2.setText(player2.getPoints() + " Points");
-		pointsPlayer3.setText(player3.getPoints() + " Points");
-		pointsPlayer4.setText(player4.getPoints() + " Points");
+		pointsPlayer1.setText(player1.getName() + ": " + player1.getPoints() + " Points");
+		pointsPlayer2.setText(player2.getName() + ": " + player2.getPoints() + " Points");
+		pointsPlayer3.setText(player3.getName() + ": " + player3.getPoints() + " Points");
+		pointsPlayer4.setText(player4.getName() + ": " + player4.getPoints() + " Points");
 		gamePanel.add(playerName1, BorderLayout.NORTH);
 		gamePanel.add(playerName2, BorderLayout.EAST);
 		gamePanel.add(playerName3, BorderLayout.SOUTH);
